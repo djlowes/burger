2c373b8e78ca23932edaf86531963ebf9ddd1e7e
djlowes <djlowes@gmail.com>
2017-11-17 12:19:06 -0800

    sm

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Baconburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Vegieburger', false);
