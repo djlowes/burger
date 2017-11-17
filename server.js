d0425ca232b1adbda1c91cfcab1096dc5a815f53
David <djlowes@gmail.com>
2017-11-17 15:47:58 -0800

    server complete, path set

--- server.js
+++ server.js
@@ -3,14 +3,14 @@
 //--------------------------------------
 var express = require("express");
 var bodyParser = require("body-parser");
-var methodOveride = require("method-override");
+var exphbs = require("express-handlebars");
 
 var app = express();
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 
-var exphbs = require("express-handlebars");
+
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set('view engine', 'handlebars');
 
