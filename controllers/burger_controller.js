62084b85f5f46421f435a14bf5705cd8600f15a8
David <djlowes@gmail.com>
2017-11-18 20:01:57 -0800

    design

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -14,15 +14,15 @@ router.post("/burgers/create", function(req, res) {
   burger.insertOne(req.body.burger_name, function(result) {
       console.log(result);
       res.redirect('/');
-  })
+  });
 });
 
 router.put('/burgers/update', function(req,res) {
     burger.updateOne(req.body.burger_id, function(result) {
         console.log(result);
         res.redirect('/');
-    })
-})
+    });
+});
 
 // Export routes for server.js to use.
 module.exports = router;
